e106169fbd389565c2345e98d39b81ec11f39a48
cindyyyyyyyyyyyyy <158173975+cindyyyyyyyyyyyyy@users.noreply.github.com>
2024-02-01 20:47:12 +0800

    Add files via upload

---
+++ trial_streamlit.py
@@ -0,0 +1,42 @@
+import streamlit as st
+
+st.write(
+    "Hello World"
+)
+
+'''
+lalalalla \n
+wewewlalalal
+rngpaoerjgperjgk[larjg[iajrg]]
+'''
+
+st.markdown(
+    'ini markdown'
+)
+st.title(
+    'ini title'
+)
+st.header(
+    'ini header'
+)
+st.subheader(
+    'ini subheader'
+)
+st.text(
+    'ini text'
+)
+st.caption(
+    'ini caption'
+)
+st.code(
+    'ini code'
+)
+st.code(
+    '''
+    ini multi code
+    # pake command
+    '''
+)
+st.latex(
+    'ax^2 + bx +- c = 0'
+)
